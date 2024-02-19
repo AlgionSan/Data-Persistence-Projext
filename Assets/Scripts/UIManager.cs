8154c33c2a1e37fe617ff2d02ac4f74ba4d04c65
Kyle Gordo <kylgordz@gmail.com>
2024-02-19 10:17:04 +0800

    Added menu

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance;
+    public string playerName;
+
+    public InputField nameInput;
+
+
+    public void OnEndEdit()
+    {
+        playerName = nameInput.text;
+        Debug.Log("Player's Name: " +  playerName);
+    
+    
+    }
+
+    
+
+    void StartGame() 
+    {
+        SceneManager.LoadScene(1);
+
+    
+    
+    }
+
+}
